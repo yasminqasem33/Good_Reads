a32b4d8fb0fa6991766b9ca5205c703ca52ac285
ArwaMohsen <arwamohsen22@gmail.com>
2019-03-06 22:28:38 +0200

    added category page

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,9 +2,17 @@ const express = require('express')
 
 
 const userRouter = express.Router()
+userRouter.get('/categories', (req,res)=>
+{
+    res.render('pages/usercategories.ejs')
+
+})
 
 userRouter.get('/',(req,res)=>{
     res.render('pages/userHome.ejs')
 })
 
+userRouter.get('/category', (req, res) =>{
+    res.redirect('/user')
+})
 module.exports=userRouter
