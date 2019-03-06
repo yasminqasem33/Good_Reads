89149abf3e1a85d53b33090866efa95bd434b711
Ali ElKhatteb <=thebasher1995@gmail.com>
2019-03-06 19:07:30 +0200

    books id page and did the routing

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,5 +6,11 @@ const userRouter = express.Router()
 userRouter.get('/',(req,res)=>{
     res.render('pages/userHome.ejs')
 })
+userRouter.get('/books/bookid',(req,res)=>{
+    res.render('pages/bookid.ejs')
+})
+userRouter.get('/books/userHome',(req,res)=>{
+    res.redirect('/user')
+})
 
 module.exports=userRouter
