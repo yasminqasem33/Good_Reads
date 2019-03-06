250a53950bcdc288b135be90b610f714e410984d
Dina Gamal <dinag707@gmail.com>
2019-03-06 21:03:31 +0200

    dina

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,11 +4,21 @@ const express = require('express')
 const userRouter = express.Router()
 
 userRouter.get('/userhome',(req,res)=>{
-    res.render('pages/userhome.ejs')
+    res.render('pages/userHome.ejs');
+    req.body.loginname="dina";
 })
 
 userRouter.get('/',(req,res)=>{
     res.render('pages/homepage.ejs')
 })
+
+userRouter.post('/',(req,res)=>
+ {
+    if(req.body.uname=="dina" && req.body.psw=="12345")
+    {
+        res.redirect('user/userhome')     
+    }
+})
+
 
 module.exports=userRouter
