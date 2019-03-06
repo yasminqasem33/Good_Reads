78e7874534a99b8ea8d9002dfb2f425374b80dda
yasmin <yasminqasem11@gmail.com>
2019-03-06 23:27:32 +0200

    routes of admin

--- models/authorModel.js
+++ models/authorModel.js
@@ -3,11 +3,27 @@ const mongoose = require('mongoose')
 
 const authorSchema =  new mongoose.Schema(
     {
-        name:{type:String},
-        
+        first_name:{type:String},
+        last_name:{type:String},
+        photo:{type:String},
+        date_birth:{type:String},
+
         
         
     }
 )
+
 const authorModel = mongoose.model('authorSchema',authorSchema)
+// authorModel.create({
+//     first_name:"author11",
+//     last_name:"ssss",
+//     photo:"dss",
+//     date_birth:"assa"
+// }, function (err, authorData) {
+//     if (err) return handleError(err);
+
+//     console.log(authorData)
+
+
+// })
 module.exports=authorModel
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,38 +1,76 @@
 const express = require('express')
+const categoryModel= require('../models/categoryModel')
+const authorModel=require('../models/authorModel')
+const bookModel=require('../models/bookModel')
 //import alert from 'alert-node'
 
 const adminRouter = express.Router()
-
-adminRouter.get('/', (req, res) => {
-    res.render('pages/adminsignin.ejs')
- });
-adminRouter.get('/homeAdmin',(req,res)=>
+//login page admin
+adminRouter.get('/', (req, res) => {    
+        res.render('pages/adminsignin.ejs')
+});
+ //homepage admin
+adminRouter.get('/home',(req,res)=>
 {
-    //console.log("fff")
-    res.render('pages/adminhome.ejs')
+    categoryModel.find()
+    .then((categories)=>
+    {
+        bookModel.find()
+        .then((books)=>
+        {
+            authorModel.find()
+            .then((authors)=>
+            {
+                //console.log(authors)
+                res.render('pages/adminhome',
+                {
+                    books:books,
+                    authors:authors,
+                    categories:categories
+                })
+    
+            })
+        })
+       
+       
+    })
+    
+    // categoryModel.find((err,categories)=>
+    // {
+    //     res.render('pages/adminhome.ejs',
+    //     {
+    //         categories:categories
+    //     })
 
+    // })
+    // authorModel.find()
+    // .then((authors)=>
+    // {
+    //     res.render('pages/adminhome.ejs',
+    //     {
+    //         authors:authors
+    //     })
+    // })
+    
 })
+//login admin validation
  adminRouter.post('/',(req,res)=>
  {
     if(req.body.name=="yasmin" && req.body.password=="12345")
     {
-        res.redirect('admin/homeAdmin')
+        res.redirect('admin/home')
          
     }
     else
     {
-    //    //window.alert("You aren't Admin")
-    //    var popupS = require('popups');
- 
-    //    popupS.alert({
-    //        content: 'Hello World!'
-    //    });
         res.redirect('/admin')
-        
     }
 
 
  })
+
+
+ 
 
 
 
