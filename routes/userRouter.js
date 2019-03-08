5ca9af385d34cb83677d11dc0d3be4b48aeae9a2
ArwaMohsen <arwamohsen22@gmail.com>
2019-03-08 17:17:37 +0200

    k

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -35,27 +35,6 @@ userRouter.get('/categories/:id', (req, res, next) => {
     })
 });
 
-// userRouter.get('/categories/:id', (req,res, next)=>
-// {
-//     categoryModel.findById(req.params.id).then((name)=>{
-
-//         res.render('pages/eco1.ejs',
-//         {
-
-//             name:name
-//         })
-//         //res.send(name.name)
-//     })
-//     bookmodel.findOne({categoryId:req.params.id}).then((record)=>{
-//         res.render('pages/eco1.ejs',
-//         {
-//             record:record
-//         })
-//     })
-
-//   //  res.render('pages/eco1.ejs')
-// })
-
 
 // userRouter.get('/categories', (req,res)=>
 // {
@@ -64,32 +43,6 @@ userRouter.get('/categories/:id', (req, res, next) => {
 // })
 // userRouter.get('/categories/:categoryid/eco1', (req, res) =>{
 //     res.render('pages/eco1.ejs')
-// })
-// userRouter.get('/categories/:categoryid/eco2', (req, res) =>{
-//     res.render('pages/eco2.ejs')
-// })
-// userRouter.get('/categories/:categoryid/eco3', (req, res) =>{
-//     res.render('pages/eco3.ejs')
-// })
-
-// userRouter.get('/categories/:categoryid/sprt1', (req, res) =>{
-//     res.render('pages/sprt1.ejs')
-// })
-// userRouter.get('/categories/:categoryid/sprt2', (req, res) =>{
-//     res.render('pages/sprt2.ejs')
-// })
-// userRouter.get('/categories/:categoryid/sprt3', (req, res) =>{
-//     res.render('pages/sprt3.ejs')
-// })
-
-// userRouter.get('/categories/:categoryid/scty1', (req, res) =>{
-//     res.render('pages/scty1.ejs')
-// })
-// userRouter.get('/categories/:categoryid/scty2', (req, res) =>{
-//     res.render('pages/scty2.ejs')
-// })
-// userRouter.get('/categories/:categoryid/scty3', (req, res) =>{
-//     res.render('pages/scty3.ejs')
 // })
 
 userRouter.get('/userhome', (req, res) => {
@@ -110,22 +63,6 @@ userRouter.post('/userhome',(req,res)=>{
 
 // userRouter.get('/categories/:categoryid/art1', (req, res) =>{
 //     res.render('pages/art1.ejs')
-// })
-// userRouter.get('/categories/:categoryid/art2', (req, res) =>{
-//     res.render('pages/art2.ejs')
-// })
-// userRouter.get('/categories/:categoryid/art3', (req, res) =>{
-//     res.render('pages/art3.ejs')
-// })
-
-// userRouter.get('/categories/:categoryid/hror1', (req, res) =>{
-//     res.render('pages/hror1.ejs')
-// })
-// userRouter.get('/categories/:categoryid/hror2', (req, res) =>{
-//     res.render('pages/hror2.ejs')
-// })
-// userRouter.get('/categories/:categoryid/:categoryid/hror3', (req, res) =>{
-//     res.render('pages/hror3.ejs')
 // })
 
 userRouter.get('/', (req, res) => {
